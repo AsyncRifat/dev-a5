49502540564a27dde8d438c879976a96fc7570cb
G0Z3RO <ibrahim3rifat@gamil.com>
2025-03-02 00:47:12 +0600

    I have done some button and js file

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
-document.getElementById('add').addEventListener('click', function () {
-  console.log('rifat');
-})
+document.getElementById('question-task').addEventListener('click', function () {
+  window.location.href = '../next-index.html';
+});
+document.getElementById('clear-btn').addEventListener('click', function () {
+  const history = document.getElementById('history');
+  history.classList.add('hidden');
+});
+document.getElementById('color-btn').addEventListener('click', function () {
+  (document.body.style.background = 'green');
+});
+
